9828d7caff66f1a7348bf3e9c4bf632b3c5110ac
urja <urja.banati@gmail.com>
2020-04-28 23:03:16 +0530

    added header with links to hone,sho,contact pages

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './App.css';
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.components.jsx";
-
+import Header from "./components/header/header.component.jsx";
 
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <switch>
       <Route exact path ="/" component={Homepage}/>
       <Route path ="/shop" component={ShopPage}/>
